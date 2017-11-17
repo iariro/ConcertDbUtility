fc2a46f9c44f6b4a9164f7bc39c62f4dc6c938ba
kumagai <iariro@gol.com>
2017-11-18 05:34:11 +0900

    例外対応

--- ConcertDbUtility/ConcertCollectionDocument.cs
+++ ConcertDbUtility/ConcertCollectionDocument.cs
@@ -6,26 +6,44 @@ using System.Xml;
 namespace ConcertDbUtility
 {
 	/// <summary>
-	/// 
+	/// コンサート情報XMLドキュメント。
 	/// </summary>
 	class ConcertCollectionDocument
 		: XmlDocument
 	{
 		/// <summary>
-		/// 
+		/// コンサート情報コレクションを取得。
 		/// </summary>
-		/// <returns></returns>
+		/// <returns>コンサート情報コレクション</returns>
 		public List<Concert> GetConcertCollection()
 		{
-			List<Concert> ret;
+			List<Concert> collections = new List<Concert>();
 
-			ret = new List<Concert>();
-			foreach(XmlElement element in DocumentElement.ChildNodes)
+			for (int i=0 ; i<DocumentElement.ChildNodes.Count ; i++)
 			{
-				ret.Add(new Concert(element));
+				XmlElement element =
+					DocumentElement.ChildNodes[i] as XmlElement;
+
+				if (element != null)
+				{
+					// 要素であった。
+
+					try
+					{
+						collections.Add(new Concert(element));
+					}
+					catch (FormatException exception)
+					{
+						throw new Exception(
+							string.Format(
+								"{0}個目のコンサート情報でエラー\r\n{1}",
+								i + 1,
+								exception.ToString()));
+					}
+				}
 			}
 
-			return ret;
+			return collections;
 		}
 	}
 }
