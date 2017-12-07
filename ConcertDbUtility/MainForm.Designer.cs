ca9fb194b1301d7360d08309f9abf183e8d3b1d4
kumagai <kumagai@toshiba-tops.co.jp>
2017-12-07 12:32:17 +0900

    ショートカットキーを追加

--- ConcertDbUtility/MainForm.Designer.cs
+++ ConcertDbUtility/MainForm.Designer.cs
@@ -32,14 +32,10 @@
             this.データベースToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.入力ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.スキーマ生成ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.URL検証ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.XMLにエクスポートToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.検証ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.重複コンサートチェックToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.日時修正ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
             this.終了ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.textBoxMessage = new ConcertDbUtility.LineTextBox();
             this.menuStrip.SuspendLayout();
@@ -51,7 +47,7 @@
             this.データベースToolStripMenuItem});
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.Name = "menuStrip";
-            this.menuStrip.Size = new System.Drawing.Size(468, 24);
+            this.menuStrip.Size = new System.Drawing.Size(468, 26);
             this.menuStrip.TabIndex = 0;
             this.menuStrip.Text = "menuStrip1";
             // 
@@ -60,98 +56,73 @@
             this.データベースToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.入力ToolStripMenuItem,
             this.スキーマ生成ToolStripMenuItem,
-            this.URL検証ToolStripMenuItem,
-            this.XMLにエクスポートToolStripMenuItem,
             this.toolStripSeparator2,
             this.検証ToolStripMenuItem,
             this.重複コンサートチェックToolStripMenuItem,
             this.toolStripSeparator1,
-            this.日時修正ToolStripMenuItem,
-            this.toolStripSeparator3,
             this.終了ToolStripMenuItem});
             this.データベースToolStripMenuItem.Name = "データベースToolStripMenuItem";
-            this.データベースToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
-            this.データベースToolStripMenuItem.Text = "データベース";
+            this.データベースToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.データベースToolStripMenuItem.Text = "データベース(&D)";
             // 
             // 入力ToolStripMenuItem
             // 
             this.入力ToolStripMenuItem.Name = "入力ToolStripMenuItem";
-            this.入力ToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.入力ToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.I)));
+            this.入力ToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
             this.入力ToolStripMenuItem.Text = "入力";
             this.入力ToolStripMenuItem.Click += new System.EventHandler(this.入力ToolStripMenuItem_Click);
             // 
             // スキーマ生成ToolStripMenuItem
             // 
             this.スキーマ生成ToolStripMenuItem.Name = "スキーマ生成ToolStripMenuItem";
-            this.スキーマ生成ToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.スキーマ生成ToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.スキーマ生成ToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
             this.スキーマ生成ToolStripMenuItem.Text = "スキーマ生成";
             this.スキーマ生成ToolStripMenuItem.Click += new System.EventHandler(this.スキーマ生成ToolStripMenuItem_Click);
-            // 
-            // URL検証ToolStripMenuItem
-            // 
-            this.URL検証ToolStripMenuItem.Name = "URL検証ToolStripMenuItem";
-            this.URL検証ToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
-            this.URL検証ToolStripMenuItem.Text = "URL検証";
-            this.URL検証ToolStripMenuItem.Click += new System.EventHandler(this.URL検証ToolStripMenuItem_Click);
-            // 
-            // XMLにエクスポートToolStripMenuItem
-            // 
-            this.XMLにエクスポートToolStripMenuItem.Name = "XMLにエクスポートToolStripMenuItem";
-            this.XMLにエクスポートToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
-            this.XMLにエクスポートToolStripMenuItem.Text = "XMLにエクスポート";
-            this.XMLにエクスポートToolStripMenuItem.Click += new System.EventHandler(this.XMLにエクスポートToolStripMenuItem_Click);
             // 
             // toolStripSeparator2
             // 
             this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new System.Drawing.Size(171, 6);
+            this.toolStripSeparator2.Size = new System.Drawing.Size(205, 6);
             // 
             // 検証ToolStripMenuItem
             // 
             this.検証ToolStripMenuItem.Name = "検証ToolStripMenuItem";
-            this.検証ToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.検証ToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.検証ToolStripMenuItem.Size = new System.Drawing.Size(256, 22);
             this.検証ToolStripMenuItem.Text = "検証";
             this.検証ToolStripMenuItem.Click += new System.EventHandler(this.検証ToolStripMenuItem_Click);
             // 
             // 重複コンサートチェックToolStripMenuItem
             // 
             this.重複コンサートチェックToolStripMenuItem.Name = "重複コンサートチェックToolStripMenuItem";
-            this.重複コンサートチェックToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.重複コンサートチェックToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D)));
+            this.重複コンサートチェックToolStripMenuItem.Size = new System.Drawing.Size(256, 22);
             this.重複コンサートチェックToolStripMenuItem.Text = "重複コンサートチェック";
             this.重複コンサートチェックToolStripMenuItem.Click += new System.EventHandler(this.重複コンサートチェックToolStripMenuItem_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(171, 6);
-            // 
-            // 日時修正ToolStripMenuItem
-            // 
-            this.日時修正ToolStripMenuItem.Name = "日時修正ToolStripMenuItem";
-            this.日時修正ToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
-            this.日時修正ToolStripMenuItem.Text = "日時修正";
-            this.日時修正ToolStripMenuItem.Click += new System.EventHandler(this.日時修正ToolStripMenuItem_Click);
-            // 
-            // toolStripSeparator3
-            // 
-            this.toolStripSeparator3.Name = "toolStripSeparator3";
-            this.toolStripSeparator3.Size = new System.Drawing.Size(171, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(205, 6);
             // 
             // 終了ToolStripMenuItem
             // 
             this.終了ToolStripMenuItem.Name = "終了ToolStripMenuItem";
-            this.終了ToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.終了ToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.終了ToolStripMenuItem.Size = new System.Drawing.Size(256, 22);
             this.終了ToolStripMenuItem.Text = "終了";
             this.終了ToolStripMenuItem.Click += new System.EventHandler(this.終了ToolStripMenuItem_Click);
             // 
             // textBoxMessage
             // 
             this.textBoxMessage.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBoxMessage.Location = new System.Drawing.Point(0, 24);
+            this.textBoxMessage.Location = new System.Drawing.Point(0, 26);
             this.textBoxMessage.Multiline = true;
             this.textBoxMessage.Name = "textBoxMessage";
             this.textBoxMessage.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBoxMessage.Size = new System.Drawing.Size(468, 267);
+            this.textBoxMessage.Size = new System.Drawing.Size(468, 265);
             this.textBoxMessage.TabIndex = 1;
             // 
             // MainForm
@@ -181,13 +152,9 @@
 		private System.Windows.Forms.ToolStripMenuItem 検証ToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem スキーマ生成ToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem 終了ToolStripMenuItem;
-		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
-		private System.Windows.Forms.ToolStripMenuItem URL検証ToolStripMenuItem;
-		private LineTextBox textBoxMessage;
-		private System.Windows.Forms.ToolStripMenuItem XMLにエクスポートToolStripMenuItem;
-		private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
-		private System.Windows.Forms.ToolStripMenuItem 日時修正ToolStripMenuItem;
-		private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private LineTextBox textBoxMessage;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
 		private System.Windows.Forms.ToolStripMenuItem 重複コンサートチェックToolStripMenuItem;
 	}
 }
